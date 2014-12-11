ca48a3d0c8b7ec840b71060af184e3fc093787fa
Johnson Diao <chengdong.diao@gm-innovation.com>
2014-12-11 17:54:09 +0800

    add git parse function

---
+++ parsegitlog.py
@@ -0,0 +1,32 @@
+import commands
+import re
+
+pattern = re.compile(r'(commit .*\nAuthor: .*\nDate: .*\n)')
+
+def ParseGitLog(log):
+    match = pattern.split(log)
+    if match:
+        if match[0] == "":
+            del match[0]
+        Iscommit = True
+        OneMessage = False
+        AllMsg = []
+	for i in range(len(match)):
+            p = match[i]
+            if Iscommit and p[:7] == "commit ":
+                commit = p[7:47]
+                commitbody = p
+                Iscommit = False
+            elif  (Iscommit == False) and (p[:7] != "commit "):
+                body = p
+                OneMessage = True
+                Iscommit = True
+            if OneMessage:
+                Message = {'commit':commit,'body':commitbody + body}
+                AllMsg.append(Message)
+                OneMessage = False
+    print "============"
+    print AllMsg
+    return AllMsg              
+err,log = commands.getstatusoutput("cd ~/gitcentral;git log")
+ParseGitLog(log)
