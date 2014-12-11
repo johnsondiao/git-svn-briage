eebe2c2b911e0607b93329af68354cce4cd8938d
Johnson Diao <chengdong.diao@gm-innovation.com>
2014-12-11 11:13:07 +0800

    could read git log

---
+++ getgitlog.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import shutil
+import commands
+ 
+def errExit(msg):
+    print "-" * 50
+    print "ERROR:"
+    print msg
+    sys.exit(1)
+ 
+def main(source_dir):
+    print "Get the log from", source_dir
+    mycmd = "cd " + source_dir + ";git log"
+    err, log = commands.getstatusoutput(mycmd)
+    #print "err = ", err
+    #print "log = ", log
+    NextCommitBegin = 0
+    AllMsg = []
+    if not err:
+        while (NextCommitBegin != -1):    
+            CommitBegin = log.find("commit ", NextCommitBegin)
+            CommitBodyBegin = CommitBegin + 7
+            CommitBodyEnd = CommitBodyBegin + 40
+            NextCommitBegin = log.find("commit ", CommitBodyEnd)
+            Body = log[CommitBegin : NextCommitBegin]
+            Commit = log[CommitBodyBegin : CommitBodyEnd]
+            Message = {'Commit': Commit,'Body': Body}
+            AllMsg.append(Message)
+    print "AllMsg=", AllMsg
+
+    
+ 
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        if "-h" in sys.argv or "--help" in sys.argv:
+            print __doc__
+            sys.exit(1)
+        errExit(u"invalid arguments!")
+    source_dir = sys.argv[1]
+    if os.path.isdir(source_dir) == False:
+        errExit(u"'%s' is not a folder!" % source_dir)
+ 
+    main(source_dir)
